c7a4bc7be6fc2358d3bebc9c53930c391a74c66b
Karaban Gleb <gleb1524@yandex.ru>
2023-01-09 16:53:31 +0300

    Initial commit

---
+++ src/test/java/ru/geekbrains/karaban/spring/WinterMarket2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.karaban.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinterMarket2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
